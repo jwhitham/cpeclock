fef00b2a88031351d3c9e39525cf867a4008f667
Jack Whitham <jack.d.whitham@gmail.com>
2023-04-15 14:41:58 +0100

    show time with fonts

--- cpeclock.ino
+++ cpeclock.ino
@@ -4,10 +4,15 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <RTClib.h>
+#include <Fonts/FreeSans12pt7b.h>
+#include <Fonts/FreeSans9pt7b.h>
 
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define BLUE_AREA_Y (SCREEN_HEIGHT / 4)
+#define LINE_1_Y ((SCREEN_HEIGHT / 2) + 2)
+#define LINE_2_Y (SCREEN_HEIGHT - 5)
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3c ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
@@ -26,13 +31,7 @@ void setup() {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("Boot");
+    Serial.println("Boot " __DATE__);
     Serial.flush();
 
     if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -41,24 +40,14 @@ void setup() {
         for(;;);
     }
     display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setFont(&FreeSans12pt7b);
+    display.setCursor(0, LINE_1_Y);
+    display.println("Boot");
+    display.setCursor(0, LINE_2_Y);
+    display.println(__DATE__);
     display.display();
-
-    Serial.println();
-    Serial.println("memory dump (EEPROM)");
-    Serial.flush();
-
-    for (int address = 0; address < 4096; address++) {
-        int v = eeprom_read(address);
-        if (v != 0xff) {
-            Serial.print(address, HEX);
-            Serial.print(" ");
-            Serial.println(v);
-        }
-    }
-    Serial.println();
-    Serial.println("end");
-    Serial.flush();
-
 
     if (!rtc.begin()) {
         Serial.println("rtc.begin() failed");
@@ -73,19 +62,14 @@ void setup() {
             for(;;);
         }
     }
-
-    Serial.println("memory dump (NVRAM)");
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    display.clearDisplay();
+    display.display();
+    Serial.println("OK");
     Serial.flush();
-
-    for (int address = 0; address < 56; address++) {
-        int v = rtc.readnvram(address);
-        if ((v != 0xff) && (v != 0x00)) {
-            Serial.print(address, HEX);
-            Serial.print(" ");
-            Serial.println(v);
-        }
-    }
 }
+
 uint8_t eeprom_read(uint16_t address)
 {
 	uint8_t first,second,data;
@@ -130,27 +114,34 @@ void eeprom_write(uint16_t address, uint8_t value)
 
 // the loop function runs over and over again forever
 void loop() {
+    char tmp[16];
+
+    // Wait for RTC to advance
+    DateTime now = rtc.now();
+    uint8_t save = now.second();
+    do {
+        delay(10);
+        now = rtc.now();
+    } while (save == now.second());
+
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+
+    // Redraw display
+    display.clearDisplay();
+    display.setFont(&FreeSans9pt7b);
+    display.setCursor(0, BLUE_AREA_Y - 1);
+    display.println("..");
+    display.setFont(&FreeSans12pt7b);
+    snprintf(tmp, sizeof(tmp), "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+    display.setCursor(0, LINE_1_Y);
+    display.println(tmp);
+    snprintf(tmp, sizeof(tmp), "%02d/%02d/%04d", now.day(), now.month(), now.year());
+    display.setCursor(0, LINE_2_Y);
+    display.println(tmp);
+    display.display();
+
+    // Flash the light
+    delay(50);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
 
-/*
-      DateTime now = rtc.now();
-
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(now.dayOfTheWeek());
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-*/
 }

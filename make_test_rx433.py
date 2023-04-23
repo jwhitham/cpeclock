bcdf3ce7bb2a1e96c71c9b50b710fc208ab2b096
Jack Whitham <jack.d.whitham@gmail.com>
2023-04-23 18:20:05 +0100

    test case for C decoder

---
+++ make_test_rx433.py
@@ -0,0 +1,17 @@
+
+from readcode import read_edges_in_zip
+import math
+
+
+def main():
+    edges1 = read_edges_in_zip("he_433_tests.zip", "20230409-0001-office-off-04022b83.csv")
+    edges2 = read_edges_in_zip("he_433_tests.zip", "20230409-0002-bed3-on-00ad6496.csv")
+
+    with open("test_rx433.txt", "wt") as fd:
+        for edges in [edges1, edges2]:
+            for (time, high) in edges:
+                if high and time >= 0.0:
+                    fd.write("{:08x}\n".format(int(math.floor(time * 1e6))))
+
+if __name__ == "__main__":
+    main()
---
+++ test_rx433.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t test_time = 0;
+extern uint32_t rx433_data;
+extern void rx433_interrupt(void);
+
+uint32_t micros()
+{
+    return test_time;
+}
+
+
+int main(void)
+{
+    FILE* fd;
+    unsigned test1_count = 0;
+    unsigned test2_count = 0;
+
+    fd = fopen("test_rx433.txt", "rt");
+    if (!fd) {
+        perror("unable to read test data");
+        return 1;
+    }
+    while (fscanf(fd, "%x\n", &test_time) == 1) {
+        rx433_data = 0;
+        rx433_interrupt();
+        switch(rx433_data) {
+            case 0:
+                break;
+            case 0x4022b83:
+                test1_count ++;
+                break;
+            case 0xad6496:
+                test2_count ++;
+                break;
+            default:
+                fprintf(stderr, "invalid code received: %08x\n", rx433_data);
+                return 1;
+        }
+    }
+    fclose(fd);
+    if ((test1_count < 15)
+    || (test2_count < 15)
+    || (test1_count > 20)
+    || (test2_count > 20)) {
+        fprintf(stderr, "incorrect results: %u %u\n", test1_count, test2_count);
+        return 1;
+    }
+    printf("ok\n");
+    return 0;
+}
+
+

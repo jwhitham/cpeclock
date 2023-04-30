40605d3740ba104087a4d995be9f45645cc8c6cc
Jack Whitham <jack.d.whitham@gmail.com>
2023-04-30 12:11:17 +0100

    message transmitter

--- test/msg.py
+++ test/msg.py
@@ -2,13 +2,36 @@
 from hmac433 import HMAC433
 import sys
 import base64
+import os
 
+
+TX433 = "/dev/tx433"
 
 def main() -> None:
+    unencoded = " ".join(sys.argv[1:]).encode("ascii")
+    if len(unencoded) == 0:
+        print("No message")
+        return
+    if len(unencoded) > (192 // 8):
+        print("Message is too long, max 192 bits")
+        return
+
     with HMAC433(b"\x01\x02\x03\x04") as hmac:
-        print(hmac.counter)
-        data = hmac.encode_packet(" ".join(sys.argv[1:]).encode("ascii"))
-        print(base64.b16encode(data))
+        data = hmac.encode_packet(unencoded)
+        print("Counter becomes: {:08x}".format(hmac.counter))
+
+    encoded = base64.b16encode(data)
+    print("To transmit:", encoded.decode("ascii"))
+    encoded += b"\n"
+    if os.path.exists(TX433):
+        try:
+            with open(TX433, "wb") as fd:
+                fd.write(encoded)
+            print("Ok")
+        except Exception:
+            print("Unable to write to", TX433)
+    else:
+        print("No device")
 
 if __name__ == "__main__":
     main()
